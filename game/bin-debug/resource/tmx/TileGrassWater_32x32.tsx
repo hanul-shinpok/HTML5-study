77ba2205252a3adb590fd62035cd5a05c34f7f68
JiAn Lee <32667619+choring0989@users.noreply.github.com>
2021-12-14 13:49:39 +0900

    add new tile

---
+++ game/bin-debug/resource/tmx/TileGrassWater_32x32.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.2" name="TileGrassWater_32x32" tilewidth="64" tileheight="64" tilecount="32" columns="8">
+ <image source="../../../tile-png/TileGrassWater_32x32.png" width="512" height="256"/>
+</tileset>
